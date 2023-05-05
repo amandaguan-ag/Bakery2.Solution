84282930d174eb40e0588b6e361edfb115388db8
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-04 21:05:11 -0700

    write get vendor properties test

--- PierresBakery.Tests/ModelTests/VendorTests.cs
+++ PierresBakery.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,18 @@ namespace PierresBakery.Tests
             Vendor newVendor = new Vendor();
             Assert.AreEqual(typeof(Vendor), newVendor.GetType());
         }
+
+        [TestMethod]
+        public void GetVendorProperties_ReturnsVendorProperties_VendorProperties()
+        {
+            Vendor newVendor = new Vendor { Id = 1, Name = "Suzie's Cafe", Description = "A cozy cafe" };
+            int id = 1;
+            string name = "Suzie's Cafe";
+            string description = "A cozy cafe";
+
+            Assert.AreEqual(id, newVendor.Id);
+            Assert.AreEqual(name, newVendor.Name);
+            Assert.AreEqual(description, newVendor.Description);
+        }
     }
 }
