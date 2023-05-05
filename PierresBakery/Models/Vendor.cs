951bbdafead33d1c3d0be6ba9e00cdda9b663428
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-04 21:06:46 -0700

    pass getvendorproperties test

--- PierresBakery/Models/Vendor.cs
+++ PierresBakery/Models/Vendor.cs
@@ -3,5 +3,10 @@ using System.Collections.Generic;
 
 namespace PierresBakery.Models
 {
-    public class Vendor{}
+    public class Vendor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
 }
