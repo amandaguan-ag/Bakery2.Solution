5e79080c76d167623c7d56f64755fc7e8d94dbe4
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-04 20:37:03 -0700

    write index method to handle HTTP GET requests to /vendors

---
+++ PierresBakery/Controllers/VendorsController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PierresBakery.Models;
+
+namespace PierresBakery.Controllers
+{
+    public class VendorsController : Controller
+    {
+        private static List<Vendor> _vendors = new List<Vendor>();
+
+        [HttpGet("/vendors")]
+        public ActionResult Index()
+        {
+            return View(_vendors);
+        }
+    }
+}
