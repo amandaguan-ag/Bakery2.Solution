f836d0652ac46a21238909cbf7551df4353f8120
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-04 21:10:26 -0700

    write get order properties test

--- PierresBakery.Tests/ModelTests/OrderTests.cs
+++ PierresBakery.Tests/ModelTests/OrderTests.cs
@@ -13,5 +13,30 @@ namespace PierresBakery.Tests
             Order newOrder = new Order();
             Assert.AreEqual(typeof(Order), newOrder.GetType());
         }
+
+        [TestMethod]
+        public void GetOrderProperties_ReturnsOrderProperties_OrderProperties()
+        {
+            Order newOrder = new Order
+            {
+                Id = 1,
+                Title = "Croissant order",
+                Description = "Freshly baked croissants",
+                Price = 12.50m,
+                Date = new DateTime(2023, 5, 4)
+            };
+
+            int id = 1;
+            string title = "Croissant order";
+            string description = "Freshly baked croissants";
+            decimal price = 12.50m;
+            DateTime date = new DateTime(2023, 5, 4);
+
+            Assert.AreEqual(id, newOrder.Id);
+            Assert.AreEqual(title, newOrder.Title);
+            Assert.AreEqual(description, newOrder.Description);
+            Assert.AreEqual(price, newOrder.Price);
+            Assert.AreEqual(date, newOrder.Date);
+        }
     }
 }
