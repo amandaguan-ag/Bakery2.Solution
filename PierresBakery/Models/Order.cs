c5d6b1eea5e243f05065477cdc4514b8a7b1ea1b
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-04 21:11:16 -0700

    pass get order properties test

--- PierresBakery/Models/Order.cs
+++ PierresBakery/Models/Order.cs
@@ -3,5 +3,12 @@ using System;
 
 namespace PierresBakery.Models
 {
-    public class Order{}
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+    }
 }
--- PierresBakery/Models/Vendor.cs
+++ PierresBakery/Models/Vendor.cs
@@ -8,5 +8,6 @@ namespace PierresBakery.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public List<Order> Orders { get; set; } = new List<Order>();
     }
 }
